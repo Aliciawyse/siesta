6d1998eaa894e1bfb33336959d276502756ef66d
Lauren <lauren.spencer007@gmail.com>
2017-10-04 10:25:08 -0400

    WIP

--- assets/javascript/app-lauren.js
+++ assets/javascript/app-lauren.js
@@ -10,62 +10,73 @@ var hash2;
 $("#submit").on("click",function(){
     //localStorage.clear();
 
-    hash1 = $("#location1").val().trim();
-    hash2 = $("#location2").val().trim();
+    hash1 = $("#firstLocation").val().trim();
+    hash2 = $("#secondLocation").val().trim();
 
     localStorage.setItem("loc1", hash1);
     localStorage.setItem("loc2", hash2);
+    $("#instaMedia").empty()
+    $("#instaMedia2").empty()
  });
 
 $("#firstLocation").val((localStorage.getItem("loc1")))
 $("#secondLocation").val((localStorage.getItem("loc2")))
 
+var myParam = location.hash.split("access_token=")[1]
+//console.log(myParam)
+
 //setting href to instagram login page
-$("#instagramData").attr("href","https://api.instagram.com/oauth/authorize/?client_id="+IGClientID+"&redirect_uri="+URILand+"&response_type=token&scope=public_content")
+$("#instagrambut").attr("href","https://api.instagram.com/oauth/authorize/?client_id="+IGClientID+"&redirect_uri="+URILand+"&response_type=token&scope=public_content")
 
-//Query the Instagram API to pull most recent tags per filter
-$("#instagramData").on("click",function(e){
-    e.preventDefault();
+var myParam = location.hash.split("access_token=")[1]
+//console.log(myParam)
 
-    var myParam = location.hash.split("access_token=")[1]
-    //console.log(myParam)
+if (myParam != null || myParam != undefined) {
+    //Query the Instagram API to pull most recent tags per filter
+    $("#instagrambut").on("click",function(e){
+        e.preventDefault();
 
-    var query = "https://api.instagram.com/v1/tags/"+hash1+"/media/recent?access_token="+myParam+"&count=10?"
+
+        var query = "https://api.instagram.com/v1/tags/"+hash1+"/media/recent?access_token="+myParam+"&count=10?"
 
 
-    console.log(query);
-    $.ajax({
-        url:query,
-        method: "GET",
-        dataType:'jsonp'
-    }).done(function(response) {
-        console.log(response);
+        console.log(query);
+        $.ajax({
+            url:query,
+            method: "GET",
+            dataType:'jsonp'
+        }).done(function(response) {
+            //console.log(response);
 
-        for (var i =0; i<response.data.length;i++){
+            for (var i =0; i<response.data.length;i++){
 
-         var imgs1 = $("<img>").attr("src", response.data[i].images.standard_resolution.url)
-         console.log("img source", imgs1)
-         $("#instaMedia").append(imgs1)
-        };
+             var imgs1 = $("<img>").attr("src", response.data[i].images.standard_resolution.url)
+             //console.log("img source", imgs1)
+             $("#instaMedia").append(imgs1)
+            };
+        });
+      
+        var query2 = "https://api.instagram.com/v1/tags/"+hash2+"/media/recent?access_token="+myParam+"&callback=?"
+
+
+        console.log(query);
+        $.ajax({
+            url:query2,
+            method: "GET",
+            dataType:'jsonp'
+        }).done(function(response) {
+            //console.log(response);
+             
+             for (var i =0; i<response.data.length;i++){
+
+            var imgs2 = $("<img>").attr("src", response.data[i].images.standard_resolution.url)
+            $("#instaMedia2").append(imgs2)
+
+            };
+        });
+
     });
-  
-    var query2 = "https://api.instagram.com/v1/tags/"+hash2+"/media/recent?access_token="+myParam+"&callback=?"
-
-
-    console.log(query);
-    $.ajax({
-        url:query2,
-        method: "GET",
-        dataType:'jsonp'
-    }).done(function(response) {
-        console.log(response);
-         
-         for (var i =0; i<response.data.length;i++){
-
-        var imgs2 = $("<img>").attr("src", response.data[i].images.standard_resolution.url)
-
-        $("#instaMedia2").append(imgs2)
-        };
-    });
-});
+}
+else {console.log("no param")
+}
 
